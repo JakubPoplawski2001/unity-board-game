a3af1727dd488a8e01890d1ad2814cf773fe7a0b
Jakub <wrx75670@student.wsb.wroclaw.pl>
2024-11-23 17:33:24 +0100

    VisualelementsNamesUpdated

--- Assets/_Scripts/Managers/MainMenuUIManager.cs
+++ Assets/_Scripts/Managers/MainMenuUIManager.cs
@@ -1,3 +1,5 @@
+using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UIElements;
@@ -29,27 +31,110 @@ public class MainMenuUIManager : MonoBehaviour
     }
     #endregion
 
-    [Header("Reference")]
+
+    #region VisualElements
     [SerializeField] UIDocument uiDocument;
 
-
-    [Header("Events")]
-    [SerializeField] public UnityEvent PlayButtonClicked;
-    [SerializeField] public UnityEvent BackButtonClicked;
-
-    // VisualElements
     VisualElement root;
     VisualElement mainMenuScreen;
     VisualElement selectGameScreen;
     VisualElement lobbyScreen;
 
+    // Main Menu Screen
     Button playButton;
     Button settingsButton;
     Button exitButton;
-    Button backButton;
+    
+    // Select Game Screen
+    Button selectGameBackButton;
+    Button createGameButton;
+    IntegerField codeInputField;
+    Button joinGameButton;
+
+    // Lobby Screen
+    Button lobbyBackButton;
+    Button lobbyStartButton;
+    
+
+    void SetupVisualElements()
+    {
+        root = uiDocument.rootVisualElement;
+
+        // Screens
+        mainMenuScreen = root.Q(USSElementNames.MAIN_MENU_SCREEN);
+        selectGameScreen = root.Q(USSElementNames.SELECT_GAME_SCREEN);
+        lobbyScreen = root.Q(USSElementNames.LOBBY_SCREEN);
+
+        // MainMenu
+        playButton = root.Q<Button>(USSElementNames.MAIN_MENU_PLAY_BUTTON);
+        settingsButton = root.Q<Button>(USSElementNames.MAIN_MENU_SETTINGS_BUTTON);
+        exitButton = root.Q<Button>(USSElementNames.MAIN_MENU_EXIT_BUTTON);
+
+        // SelectGame
+        selectGameBackButton = root.Q<Button>(USSElementNames.SELECT_GAME_BACK_BUTTON);
+        createGameButton = root.Q<Button>(USSElementNames.SELECT_GAME_CREATE_GAME_BUTTON);
+        codeInputField = root.Q<IntegerField>(USSElementNames.SELECT_GAME_CODE_INPUT_FIELD);
+        joinGameButton = root.Q<Button>(USSElementNames.SELECT_GAME_JOIN_GAME_BUTTON);
+
+        // Lobby
+        lobbyBackButton = root.Q<Button>(USSElementNames.LOBBY_BACK_BUTTON);
+        lobbyStartButton = root.Q<Button>(USSElementNames.LOBBY_START_BUTTON);
+    }
+
+    #endregion
 
 
-    // MonoBehaviour methods
+    #region Events
+    [Header("Events")]
+    [SerializeField] public UnityEvent PlayButtonClicked;
+    [SerializeField] public UnityEvent ExitButtonClicked;
+    [SerializeField] public UnityEvent SelectGameBackButtonClicked;
+
+    void SubscribeToEvents()
+    {
+        playButton.clicked += OnPlayBtnClicked;
+        exitButton.clicked += OnExitBtnClicked;
+        selectGameBackButton.clicked += OnBackBtnClicked;
+    }
+
+    void UnsubscribeToEvents()
+    {
+        playButton.clicked -= OnPlayBtnClicked;
+        exitButton.clicked += OnExitBtnClicked;
+        selectGameBackButton.clicked -= OnBackBtnClicked;
+    }
+
+
+    // Event callbacks?
+    void OnPlayBtnClicked()
+    {
+        PlayButtonClicked?.Invoke();
+
+        ShowElement(selectGameScreen);
+        HideElement(mainMenuScreen);
+    }
+
+    void OnExitBtnClicked()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
+    }
+
+    void OnBackBtnClicked()
+    {
+        SelectGameBackButtonClicked?.Invoke();
+
+        ShowElement(mainMenuScreen);
+        HideElement(selectGameScreen);
+        HideElement(lobbyScreen);
+    }
+
+    #endregion
+
+
+    #region MonoBehaviour
     void Awake()
     {
         SetupSingleton();
@@ -69,59 +154,7 @@ public class MainMenuUIManager : MonoBehaviour
     {
         UnsubscribeToEvents();
     }
-
-
-
-    void SetupVisualElements()
-    {
-        root = uiDocument.rootVisualElement;
-
-        // Screens
-        mainMenuScreen = root.Q(USSElementNames.MAIN_MENU_SCREEN);
-        selectGameScreen = root.Q(USSElementNames.SELECT_GAME_SCREEN);
-        lobbyScreen = root.Q(USSElementNames.LOBBY_SCREEN);
-
-        // MainMenu
-        playButton = root.Q<Button>(USSElementNames.PLAY_BUTTON);
-        settingsButton = root.Q<Button>(USSElementNames.SETTINGS_BUTTON);
-        exitButton = root.Q<Button>(USSElementNames.EXIT_BUTTON);
-
-        // SelectGame
-        backButton = root.Q<Button>(USSElementNames.BACK_BUTTON);
-    }
-
-    void SubscribeToEvents()
-    {
-        playButton.clicked += OnPlayBtnClicked;
-        backButton.clicked += OnBackBtnClicked;
-    }
-
-    void UnsubscribeToEvents()
-    {
-        playButton.clicked -= OnPlayBtnClicked;
-        backButton.clicked -= OnBackBtnClicked;
-    }
-
-
-    // Event callbacks?
-    void OnPlayBtnClicked()
-    {
-        PlayButtonClicked?.Invoke();
-
-        ShowElement(selectGameScreen);
-        HideElement(mainMenuScreen);
-    }
-
-    void OnBackBtnClicked()
-    {
-        BackButtonClicked?.Invoke();
-
-        ShowElement(mainMenuScreen);
-        HideElement(selectGameScreen);
-        HideElement(lobbyScreen);
-    }
-
-
+    #endregion
 
 
     void ShowElement(VisualElement visualElement)
--- Assets/_Scripts/StringNames.cs
+++ Assets/_Scripts/StringNames.cs
@@ -8,13 +8,18 @@
 public static class USSElementNames
 {
     public const string MAIN_MENU_SCREEN = "MainMenuScreen";
-    public const string PLAY_BUTTON = "PlayButton";
-    public const string SETTINGS_BUTTON = "SettingsButton";
-    public const string EXIT_BUTTON = "ExitButton";
+    public const string MAIN_MENU_PLAY_BUTTON = "MainMenu_PlayButton";
+    public const string MAIN_MENU_SETTINGS_BUTTON = "MainMenu_SettingsButton";
+    public const string MAIN_MENU_EXIT_BUTTON = "MainMenu_ExitButton";
 
     public const string SELECT_GAME_SCREEN = "SelectGameScreen";
-    public const string BACK_BUTTON = "BackButton";
+    public const string SELECT_GAME_BACK_BUTTON = "SelectGame_BackButton";
+    public const string SELECT_GAME_CREATE_GAME_BUTTON = "SelectGame_CreateGameButton";
+    public const string SELECT_GAME_CODE_INPUT_FIELD = "SelectGame_CodeInputField";
+    public const string SELECT_GAME_JOIN_GAME_BUTTON = "SelectGame_JoinGameButton";
 
 
     public const string LOBBY_SCREEN = "LobbyScreen";
+    public const string LOBBY_BACK_BUTTON = "Lobby_BackButton";
+    public const string LOBBY_START_BUTTON = "Lobby_StartButton";
 }
