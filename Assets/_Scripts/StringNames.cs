24244b586aaaac234b707c39086d1bb49139c564
Jakub <wrx75670@student.wsb.wroclaw.pl>
2024-11-24 20:27:12 +0100

    AddedPlayerCardTemplate

--- Assets/_Scripts/StringNames.cs
+++ Assets/_Scripts/StringNames.cs
@@ -8,15 +8,15 @@
 public static class USSElementNames
 {
     public const string MAIN_MENU_SCREEN = "MainMenuScreen";
-    public const string MAIN_MENU_PLAY_BUTTON = "MainMenu_PlayButton";
-    public const string MAIN_MENU_SETTINGS_BUTTON = "MainMenu_SettingsButton";
-    public const string MAIN_MENU_EXIT_BUTTON = "MainMenu_ExitButton";
+    public const string MAIN_MENU_PLAY_BUTTON = "PlayButton";
+    public const string MAIN_MENU_SETTINGS_BUTTON = "SettingsButton";
+    public const string MAIN_MENU_EXIT_BUTTON = "ExitButton";
 
     public const string SELECT_GAME_SCREEN = "SelectGameScreen";
     public const string SELECT_GAME_BACK_BUTTON = "BackButton";
-    public const string SELECT_GAME_CREATE_GAME_BUTTON = "SelectGame_CreateGameButton";
-    public const string SELECT_GAME_CODE_INPUT_FIELD = "SelectGame_CodeInputField";
-    public const string SELECT_GAME_JOIN_GAME_BUTTON = "SelectGame_JoinGameButton";
+    public const string SELECT_GAME_CREATE_GAME_BUTTON = "CreateGameButton";
+    public const string SELECT_GAME_CODE_INPUT_FIELD = "CodeInputField";
+    public const string SELECT_GAME_JOIN_GAME_BUTTON = "JoinGameButton";
 
 
     public const string LOBBY_SCREEN = "LobbyScreen";
